638fe6ec31ad9b556db0162324382e930eeb07d1
patrick quan <p.quan@me.com>
2023-03-08 17:05:28 -0700

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return (
+    <div className='container mx-auto'>
+      <h1>Wooloo 🐑</h1>
+    </div>
+  )
 }
 
 export default App
