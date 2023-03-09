439bcc07ed3147f9173477d14066f4deb87f9669
patrick quan <p.quan@me.com>
2023-03-08 20:22:43 -0700

    working state

---
+++ src/components/MovieResult.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { CgFilm, CgGames, CgPlayButtonO, CgTv } from 'react-icons/cg'
+
+export interface MovieResultType {
+  Poster: string
+  Title: string
+  Type: string
+  Year: string
+  imdbID: string
+}
+
+export const MovieResult = ({ Poster, Title, Type, Year, imdbID }: MovieResultType) => {
+  const renderType = (type: string) => {
+    switch (type) {
+      case 'series':
+        return <CgTv size={300} />
+      case 'movie':
+        return <CgFilm size={300} />
+      case 'game':
+        return <CgGames size={300} />
+      default:
+        return <CgPlayButtonO size={300} />
+    }
+  }
+
+  return (
+    <Link to={imdbID}>
+      <div className='relative my-2 h-60 overflow-hidden rounded bg-slate-300 p-3 shadow'>
+        <div className='flex h-full justify-between'>
+          <img src={Poster} className='h-full' />
+          <div className='mr-auto'>
+            <h6 className='text-lg font-thin'>{Title}</h6>
+            <div className='text-sm'>({Year})</div>
+          </div>
+          <span className='absolute right-0 top-1/2 -translate-y-2/4 translate-x-1/4	text-slate-400 opacity-30'>
+            {renderType(Type)}
+          </span>
+        </div>
+      </div>
+    </Link>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,47 @@
-import React, {useState} from "react"
+import React, { useState } from 'react'
+import { CgSearch } from 'react-icons/cg'
 
-import { Input } from "components/Input"
+import { Input } from 'components/Input'
+import { MovieResultType, MovieResult } from 'components/MovieResult'
 
 export const HomePage = () => {
-  const [searchVal, setSearchVal] = useState<string>("")
+  const [searchVal, setSearchVal] = useState<string>('')
+  const [foundMovies, setFoundMoives] = useState<MovieResultType[]>([])
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchVal(e.target.value)
   }
 
+  const handleSearch = async (e: React.FormEvent) => {
+    e.preventDefault()
+    
+    const omdb_api = new URL("http://www.omdbapi.com/")
+    omdb_api.searchParams.set("apikey", process.env.REACT_APP_OMDB_API_KEY as string)
+    omdb_api.searchParams.set("s", searchVal)
+
+    const res = await fetch(omdb_api)
+    if (res.ok) {
+      const data = await res.json()
+      setFoundMoives(data.Search)
+    }
+  }
+
   return (
-    <section className="p-4">
-      <h3 className="text-2xl font-semibold text-center mt-32 mb-12">Search for a movie or show</h3>
-      <Input 
-        value={searchVal}
-        onChange={handleChange}
-        placeholder="Last Of Us..."
-        big
-      />
+    <section className='p-4'>
+      <h3 className='mt-32 mb-12 text-center text-2xl font-semibold'>Search for a movie or show</h3>
+      <div className='relative mb-10'>
+        <form onSubmit={handleSearch}>
+          <Input value={searchVal} onChange={handleChange} placeholder='The Last Of Us...' big />
+          <button type='submit'>
+            <CgSearch size='30' className={`${searchVal ? 'text-slate-800' : 'text-slate-400'} absolute right-5 top-3.5`} />
+          </button>
+        </form>
+      </div>
+      {
+        foundMovies && <div>
+          {foundMovies.map((movie) => <MovieResult key={movie.imdbID} {...movie}/>)}
+        </div>
+      }
     </section>
   )
 }
